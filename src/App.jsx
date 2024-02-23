775a1f512099d762b324dc874e1297e1454c37c0
zsoolti8917 <zsolt.varju.rl@gmail.com>
2024-02-23 15:02:41 +0100

    Changed map component to use OMS

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Nav from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
 import Banner from './components/Banner.jsx'
+import Map from './components/Map.jsx'
 function App() {
   return (
     <div>
       <Nav />
       <Hero />
       <Banner />
+      <Map />
     </div>
   )
 }
