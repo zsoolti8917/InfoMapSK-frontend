61e8a1fd541527a663f6ba8b9af24eac31a4f4cb
zsoolti8917 <zsolt.varju.rl@gmail.com>
2024-02-26 19:14:43 +0100

    Added component to visualize incoming data from the server

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Hero from './components/Hero.jsx'
 import Banner from './components/Banner.jsx'
 import Footer from './components/Footer.jsx'
 import DataHolder from './components/DataHolder.jsx'
+import DataFetcher from './components/DataFetcher.jsx'
 function App() {
   return (
     <div>
       <Nav />
       <Hero />
+      <DataFetcher />
       <DataHolder />
       <Banner />
       <Footer />
