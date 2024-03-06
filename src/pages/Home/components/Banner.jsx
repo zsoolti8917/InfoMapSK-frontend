dd594442e74c5bf02f8bd02f68535087b9a2c17c
zsoolti8917 <zsolt.varju.rl@gmail.com>
2024-03-06 10:35:52 +0100

    Finished home-hero components

--- src/pages/Home/components/Banner.jsx
+++ src/pages/Home/components/Banner.jsx
@@ -1,4 +1,3 @@
-import SearchForm from './SearchForm'
 import BgLogo from '../../../assets/bg-pattern-home-6-about-5.svg'
 const Banner = () => {
   return (
@@ -6,9 +5,7 @@ const Banner = () => {
       <div className='m-auto px-10 text-center flex flex-col gap-6 items-center lg:flex-row lg:justify-between lg:max-w-[800px] lg:w-full lg:px-0 desktop:max-w-container'>
         <h3 className='text-4xl font-extrabold lg:text-h2 flex-1 desktop:text-start'>
         Ste pripravení začať?
-        </h3>
-          
-        <SearchForm />
+        </h3>    
       </div>
       <img src={BgLogo} alt="logo" className='absolute bottom-[-20px] md:bottom-0 left-0'/>
     </section>
--- src/pages/Home/components/Hero.jsx
+++ src/pages/Home/components/Hero.jsx
@@ -1,17 +1,18 @@
-import bgGraph from '../../../assets/bg-pattern-about-1-mobile-nav-1.svg'
-
+import bgGraph1 from '../../../assets/bg-pattern-home-2.svg'
+import bgGraph2 from '../../../assets/bg-pattern-home-1.svg'
 const Hero = () => {
   return (
     <section className='bg-primary-700 w-full md:min-h-fit min-h-96 md:pt-38 pt-32 relative z-[-1] overflow-hidden pb-32 lg:pb-20'>
-        <div className='max-w-[500px] lg:max-w-[800px] desktop:max-w-container m-auto text-primary-100 font-livvic flex flex-col gap-4 lg:gap-12 text-center px-4 lg:px-0 md:gap-8 lg:flex-row lg:text-left'>
-            <h1 className='md:text-[64px] text-4xl font-bold md:leading-none lg:leading-[1.2]'>Vizualizované údaje</h1>
+        <div className='max-w-[500px] lg:max-w-[800px] desktop:max-w-container m-auto text-primary-100 font-livvic flex flex-col gap-4 lg:gap-12 text-center px-4 lg:px-0 md:gap-8 lg:flex-row lg:text-left pb-6'>
+            <h1 className='md:text-[64px] text-4xl font-bold md:leading-none lg:leading-[1.2]'>Preskúmajte Slovensko cez otvorené dáta</h1>
             <div className='flex items-center relative'>
             <div className='absolute w-[44px] h-[5px] bg-primary-300 top-0 left-0 hidden lg:block'></div>
-            <p className='text-body2'>Pozrite sa na podrobné údaje, ktoré prinášame do popredia s našou interaktívnou platformou. Tu sú najnovšie štatistiky pre vybraný región alebo kategóriu.</p>
+            <p className='text-body2'>Priblíženie komplexných dátových súborov k občanom s použitím interaktívnej vizualizácie a analytických nástrojov.</p>
             </div>
             
         </div>
-        <img src={bgGraph} alt="Graph" className='absolute right-[-100px] bottom-[-100px] md:bottom-0'/>
+        <img src={bgGraph1} alt="Graph" className='absolute bottom-0 right-1/2 transform translate-x-1/2 lg:-translate-x-0 lg:right-0'/>
+        <img src={bgGraph2} alt="Graph" className='hidden absolute lg:block -left-24 top-1/2 transform -translate-y-1/2' />
     </section>
   )
 }
