df11a5cec4d7bab334beda500f49acbe3016498a
zsoolti8917 <zsolt.varju.rl@gmail.com>
2024-03-08 20:38:29 +0100

    Added routing and scroll to top functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,10 @@ import Data from './pages/Data/Data.jsx'
 import Home from './pages/Home/Home.jsx'
 import About from './pages/About/About.jsx'
 import FAQ from './pages/FAQ/FAQ.jsx'
-
 function App() {
   return (
     <div>
-      <FAQ />
+      <Home />
     </div>
   )
 }
