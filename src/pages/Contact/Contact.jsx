618dd3377414dd9dd3dc5c4f1ec1c42791895cee
zsoolti8917 <zsolt.varju.rl@gmail.com>
2024-03-09 07:03:43 +0100

    changed path for Content.jsx

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from '../../components/Navbar'
 import Footer from '../../components/Footer'
-import Content from '../Contact/components/Content'
+import Content from './components/Content.jsx'
 const Contact = () => {
   return (
     <>
